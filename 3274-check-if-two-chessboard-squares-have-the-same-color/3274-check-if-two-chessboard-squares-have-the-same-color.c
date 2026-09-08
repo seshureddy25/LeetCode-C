0a6c4d1c7c654ba0c6bc07b253660387d5249646
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-08 07:58:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (57.14%) - LeetHub

---
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.c
@@ -0,0 +1,9 @@
+bool checkTwoChessboards(char* coordinate1, char* coordinate2) {
+        int col1 = coordinate1[0] - 'a' + 1;
+        int row1 = coordinate1[1] - '0';
+
+        int col2 = coordinate2[0] - 'a' + 1;
+        int row2 = coordinate2[1] - '0';
+
+        return (col1 + row1) % 2 == (col2 + row2) % 2;
+}
