11decb62916f728e26b7dbbda1cce7d50cb99aed
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-13 09:20:21 +0530

    Time: 0 ms (100%), Space: 9.2 MB (97.27%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <limits.h>
+
+int myAtoi(char* s)
+{
+    int i = 0;
+    long long rem = 0;
+    int sign = 1;
+    while (s[i] == ' ')
+        i++;
+
+    if (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (isdigit((unsigned char)s[i]))
+    {
+        rem = rem * 10 + (s[i] - '0');
+        if (sign == 1 && rem > INT_MAX)
+            return INT_MAX;
+        if (sign == -1 && rem > 2147483648LL)
+            return INT_MIN;
+        i++;
+    }
+    return sign * rem;
+}
