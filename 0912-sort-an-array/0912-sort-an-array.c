5df779fb891fc870aceb7a9c82a1c8134215b996
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-20 14:45:34 +0530

    Time: 343 ms (8.64%), Space: 156.3 MB (5.37%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.c
+++ 0912-sort-an-array/0912-sort-an-array.c
@@ -1,19 +1,14 @@
-#include <stdlib.h>
-
 void merge(int *nums, int left, int mid, int right)
 {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
     int *L = malloc(n1 * sizeof(int));
     int *R = malloc(n2 * sizeof(int));
-
     for(int i = 0; i < n1; i++)
         L[i] = nums[left + i];
 
     for(int i = 0; i < n2; i++)
         R[i] = nums[mid + 1 + i];
-
     int i = 0;
     int j = 0;
     int k = left;
@@ -25,7 +20,6 @@ void merge(int *nums, int left, int mid, int right)
         else
             nums[k++] = R[j++];
     }
-
     while(i < n1)
         nums[k++] = L[i++];
 
@@ -40,25 +34,18 @@ void mergeSort(int *nums, int left, int right)
 {
     if(left >= right)
         return;
-
     int mid = left + (right - left) / 2;
-
     mergeSort(nums, left, mid);
     mergeSort(nums, mid + 1, right);
-
     merge(nums, left, mid, right);
 }
 
 int* sortArray(int* nums, int numsSize, int* returnSize)
 {
     mergeSort(nums, 0, numsSize - 1);
-
     int *result = malloc(numsSize * sizeof(int));
-
     for(int i = 0; i < numsSize; i++)
         result[i] = nums[i];
-
     *returnSize = numsSize;
-
     return result;
 }
