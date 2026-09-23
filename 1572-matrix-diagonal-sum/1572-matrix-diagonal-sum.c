4ddfb94e35e65cfca59a57541d67fcd1f07bf15e
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-23 10:57:01 +0530

    Time: 0 ms (100%), Space: 10.9 MB (20.43%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.c
@@ -0,0 +1,12 @@
+int diagonalSum(int** mat, int matSize, int* matColSize) {
+    int sum=0;
+    for(int i=0;i<matSize;i++)
+    {
+        for(int j=0;j<matSize;j++)
+        {
+            if(i==j||j+i==(matSize-1))
+                sum+=mat[i][j];
+        }
+    }
+    return sum;
+}
