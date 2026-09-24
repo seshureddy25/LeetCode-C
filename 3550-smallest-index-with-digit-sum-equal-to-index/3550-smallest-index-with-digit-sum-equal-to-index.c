f4516109c3c3b4e9b8288c2cfddab0736b1e4c87
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-24 06:58:06 +0530

    Time: 0 ms (100%), Space: 9.9 MB (75%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.c
@@ -0,0 +1,15 @@
+int smallestIndex(int* nums, int numsSize) {
+    for(int i=0;i<numsSize;i++)
+    {
+        int n=nums[i];
+        int sum=0;
+        while(n>0)
+        {
+            sum+=n%10;
+            n/=10;
+        }
+        if(sum==i)
+            return i;
+    }
+    return -1;
+}
