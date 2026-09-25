5f60333c399a39d0054bfd7feb898015887452a9
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-25 07:48:13 +0530

    Time: 0 ms (100%), Space: 9.5 MB (74.59%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.c
@@ -0,0 +1,17 @@
+bool repeatedSubstringPattern(char* s) {
+    int n = strlen(s);
+    for (int len = 1; len <= n / 2; len++) {
+        if (n % len != 0)
+            continue;
+        bool valid = true;
+        for (int i = len; i < n; i++) {
+            if (s[i] != s[i % len]) {
+                valid = false;
+                break;
+            }
+        }
+        if (valid)
+            return true;
+    }
+    return false;
+}
